ab34f1cdf93dfc5b72c49fd565092038b15575a7
viniisaints <vinicios-2000@hotmail.com>
2025-03-20 19:39:57 -0300

    testeteste

---
+++ main.js
@@ -0,0 +1,6 @@
+// Tipos de variaveis
+var a = 5 // escopo global, disponivel  para geral
+let c = "teste" // escopo local só funciona quando é chamada
+const b = 10 // constante sempre vai ser o mesmo valor
+
+
