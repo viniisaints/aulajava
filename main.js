e7d5fd435ea42024b3399542ca11f5b4b5483586
viniisaints <vinicios-2000@hotmail.com>
2025-03-20 20:48:07 -0300

    laçoderepetição

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
-var a =5
-var b = 10
+const array = [2,3,4,5,6];//criand array
 
-function soma (x,y){
+array.forEach(item =>{//para cada item do array faça (item)
+    if (item % 2 === 0){//se for par...
+    console.log(`O número ${item} é par`);//... faça isso
+    }
+    else{//se não for...
+        console.log(`O número ${item} é ímpar`);//... faça isso
+    }
 
-    let sum =x+y
-    return sum
-}
-
-console.log(soma(a,b))
-
-const sub = (a,b) => {
-
-    return 5-3;
-}
-console.log(sub)
+});
