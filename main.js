3e26d34a6708130a47cd5a45074191f911c74feb
viniisaints <vinicios-2000@hotmail.com>
2025-03-20 21:23:21 -0300

    manipulacaoarray

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
-/*function Pessoa (a,b){
-    this.nome = a;
-    this.idade = b;
- }
- const x = new Pessoa('Afonso', 31)
- const y = new Pessoa ('Marcos', 20)
- console.log(x);
- console.log(y);
- */
- 
+var lista =['Gustavo', 'Henrique', "Marcos", "Afonso"];
+console.log(lista);
 
- function Carro (a,b,c,d,e){
-    this.modelo = a;
-    this.cor = b;
-     this.ano = c;
-     this.valor = d;
-     this.popular = e;
- }
- 
-var carro = new Carro('polo', 'preta', 2025, 80000, true )
+console.log(lista[1]);
 
- console.log(carro);
+lista.push('Raphinha');
+console.log (lista);
 
+lista.pop();
+console.log(lista);
+
+console.log(lista.length);
